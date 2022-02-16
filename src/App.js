3e5075517dad5a612b9c84270454e3999d6cf9c2
Lucio-jose <luciocanganjo2@gmail.com>
2022-02-16 15:59:15 +0100

    all config react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <p>Eu sou o Lúcio José</p>
+    </>
+  );
+}
+
+export default App;
